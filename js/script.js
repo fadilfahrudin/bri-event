dd05e9be3dcaefbc3080459cc22eff6a9272f8fa
Fadil Fahrudin <fadilfahrudin32@gmail.com>
2023-12-18 17:14:18 +0700

    nav active

--- js/script.js
+++ js/script.js
@@ -16,13 +16,30 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 
+    $('#nav-section .nav-section a').each(function (i, nav) {
+        $(nav).click(() => {
+            let target = $(this).attr('target')
+            let to = $(target)
+            $(".active").each(function (i, item) {
+                item.classList.remove("active");
+            })
+            $(nav).addClass('active')
+            if (to.length > 0) {
+                window.scrollTo({
+                    top: to.offset().top - 80,
+                    left: 0,
+                    behavior: 'smooth'
+                })
+            }
+        })
+    })
     $('header ul li a').each(function (i, nav) {
         $(nav).click(() => {
             let target = $(this).attr('target')
             let to = $(target)
             if (to.length > 0) {
                 window.scrollTo({
-                    top: to.offset().top,
+                    top: to.offset().top - 80,
                     left: 0,
                     behavior: 'smooth'
                 })
